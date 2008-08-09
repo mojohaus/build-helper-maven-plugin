60c0f87948985547511e0c0af6786e412c960117
Dan Tran <dantran@gmail.com>
2008-08-09 17:06:31 +0000

    add a test to prove that surefire can pickup maven project properties thru env variables
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/build-helper-maven-plugin@7389 52ab4f32-60fc-0310-b215-8acea882cd1b

--- src/it/surefire/src/test/java/org/codehaus/mojo/PassReservedPortToSureFireTest.java
+++ src/it/surefire/src/test/java/org/codehaus/mojo/PassReservedPortToSureFireTest.java
@@ -1,5 +1,7 @@
 package org.codehaus.mojo;
 
+import java.io.File;
+
 import junit.framework.TestCase;
 
 public class PassReservedPortToSureFireTest
@@ -17,6 +19,16 @@ public class PassReservedPortToSureFireTest
     {
         String port = System.getenv( "port1" );
         Integer.parseInt( port );
+    }
+
+    public void testCheckEnvironmentUsingMavenProject()
+        throws Exception
+    {
+        String env = System.getenv( "targetDir" );
+        
+        String expectEnv = new File( System.getProperty( "basedir", "." ), "target" ).getAbsolutePath();
+        
+        assertEquals( expectEnv, env );
     }
 
 }
