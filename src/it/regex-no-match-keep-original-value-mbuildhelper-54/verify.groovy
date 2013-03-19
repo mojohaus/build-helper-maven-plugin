3a109d9638fc1cd1ef8efe1b7f4f365beaa0e128
Baptiste Mathus <batmat@batmat.net>
2013-03-19 21:39:02 +0000

    Added an IT showing that having no match leaves the original value as-is.
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/build-helper-maven-plugin@18055 52ab4f32-60fc-0310-b215-8acea882cd1b

---
+++ src/it/regex-no-match-keep-original-value-mbuildhelper-54/verify.groovy
@@ -0,0 +1,7 @@
+File file = new File( basedir, "build.log" );
+assert file.exists();
+
+String text = file.getText("utf-8");
+
+assert text.contains("define property propname = \"originalvalue\"")
+return true;
