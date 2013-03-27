cd48a9a63e9dcd14770fcd348d9ca2e6427f3fd2
Baptiste Mathus <batmat@batmat.net>
2013-03-27 14:19:59 +0000

    Reverting previous commits [1]. This fix was too ugly and is actually fixed in Maven 3.1 as of MNG-5380.
    See MBUILDHELPER-34
    [1] http://mojo.10943.n7.nabble.com/Re-mojo-scm-18073-trunk-mojo-build-helper-maven-plugin-src-A-bit-more-natural-I-guess-replace-unbrea-td39333.html.
    
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/build-helper-maven-plugin@18077 52ab4f32-60fc-0310-b215-8acea882cd1b

--- src/it/mbuildhelper-53-somespaces/verify.groovy
+++
@@ -1,8 +0,0 @@
-File file = new File( basedir, "build.log" );
-assert file.exists();
-
-String text = file.getText("utf-8");
-
-assert text.contains('define property propname = "hi    "') , "text=<"+text+">"
-
-return true;
