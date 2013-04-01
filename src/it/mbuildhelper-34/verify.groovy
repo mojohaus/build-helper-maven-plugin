5080bca5238ea5143406a78195581bbdc187a4c0
Baptiste Mathus <batmat@batmat.net>
2013-04-01 14:31:41 +0000

    IT for MBUILDHELPER-34 (will be closed as WONTFIX as this is a Maven core issue).
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/build-helper-maven-plugin@18145 52ab4f32-60fc-0310-b215-8acea882cd1b

---
+++ src/it/mbuildhelper-34/verify.groovy
@@ -0,0 +1,8 @@
+File file = new File( basedir, "build.log" );
+assert file.exists();
+
+String text = file.getText("utf-8");
+
+assert text.contains('define property propname = "chapi     bidiba"')
+
+return true;
