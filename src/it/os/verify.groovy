5216552188b90bb998168cfe2b0da151c4e570cc
Karl Heinz Marbaise <khmarbaise@apache.org>
2013-04-02 10:48:54 +0000

    [MBUILDHELPER-56]   - improved IT with checks.
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/build-helper-maven-plugin@18149 52ab4f32-60fc-0310-b215-8acea882cd1b

--- src/it/os/verify.groovy
+++ src/it/os/verify.groovy
@@ -3,9 +3,11 @@ assert file.exists();
 
 String text = file.getText("utf-8");
 
-//Currently a test can only check the output of ant plugin
-//but not the function directory, cause
-//this would make the whole IT os dependant.
+assert text =~ /\[DEBUG\] define property os\.name = "(.*)"/
+assert text =~ /\[DEBUG\] define property os\.family = "(.*)"/
+assert text =~ /\[DEBUG\] define property os\.version = "(.*)"/
+assert text =~ /\[DEBUG\] define property os\.arch = "(.*)"/
+
 assert text.contains("[echo] os.name=")
 assert text.contains("[echo] os.version=")
 assert text.contains("[echo] os.family=")
