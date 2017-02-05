68c9b88e263c08bf57d7decfa341f7d0acfae039
Karl Heinz Marbaise <khmarbaise@apache.org>
2017-02-05 22:15:24 +0100

    Cleaned up source removed unused things.

--- src/main/java/org/codehaus/mojo/buildhelper/versioning/DefaultVersioning.java
+++ src/main/java/org/codehaus/mojo/buildhelper/versioning/DefaultVersioning.java
@@ -1,26 +1,5 @@
 package org.codehaus.mojo.buildhelper.versioning;
 
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
-import org.apache.maven.artifact.Artifact;
-
 /**
  * @author Karl Heinz Marbaise <a href="mailto:khmarbaise@apache.org">khmarbaise@apache.org</a>
  */
@@ -32,33 +11,16 @@ public class DefaultVersioning
 
     private String version;
 
-    private boolean snapshot;
-
     public DefaultVersioning( String version )
     {
         this.version = version;
-//        if ( version.endsWith( "-" + Artifact.SNAPSHOT_VERSION ) )
-//        {
-//            this.vi =
-//                new VersionInformation( version.substring( 0, version.length() - Artifact.SNAPSHOT_VERSION.length() ) );
-//            this.snapshot = true;
-//        }
-//        else
-//        {
-            this.vi = new VersionInformation( version );
-//            this.snapshot = false;
-//        }
+        this.vi = new VersionInformation( version );
 
     }
 
     public String getVersion()
     {
         return this.version;
-    }
-
-    public boolean isSnapshot()
-    {
-        return this.snapshot;
     }
 
     @Override
--- src/main/java/org/codehaus/mojo/buildhelper/versioning/Versioning.java
+++ src/main/java/org/codehaus/mojo/buildhelper/versioning/Versioning.java
@@ -3,8 +3,6 @@ package org.codehaus.mojo.buildhelper.versioning;
 public interface Versioning
 {
 
-    boolean isSnapshot();
-    
     int getMajor();
 
     int getMinor();
