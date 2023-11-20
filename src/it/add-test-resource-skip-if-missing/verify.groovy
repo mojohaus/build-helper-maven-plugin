a0d6d16ccba8b19f7fa75e8d9f39a2618d309e8d
Slawomir Jaranowski <s.jaranowski@gmail.com>
2023-11-20 22:06:50 +0100

    Fix ITs on windows

--- src/it/add-test-resource-skip-if-missing/verify.groovy
+++ src/it/add-test-resource-skip-if-missing/verify.groovy
@@ -3,10 +3,8 @@ assert file.exists();
 
 String text = file.getText("utf-8");
 
-
-
 assert text.contains("skipAddTestResourceIfMissing = true");
 assert text.contains("Skipping directory: ");
-assert text.contains("build-helper-maven-plugin/target/it/add-test-resource-skip-if-missing/not-existing, because it does not exist.");
+assert text.contains("not-existing, because it does not exist.");
 
 return true;
